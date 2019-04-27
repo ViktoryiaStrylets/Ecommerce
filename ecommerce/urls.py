d64ba1d10912599f0cec12eb8ec4195f74af741a
DESKTOP-859DUJQ\vikus <viktoryia.strylets@stonybrook.edu>
2019-04-26 20:47:31 -0400

    The login and register forms are added

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -14,12 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 from users import views as user_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', user_views.register, name='register'),
-    path('login/', user_views.login, name='login'),
+    path('login_page/', user_views.login_page, name='login_page'),
     path('', include('blog.url')),
     path('shop/', include('shop.urls')),
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,5 @@
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
@@ -8,14 +10,35 @@ def register(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
+            form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'Account created for {username}!')
             return redirect('blog-home')
     else:
         form = UserCreationForm()
-        return render(request, 'users/register.html', {'form': form})
+
+    return render(request, 'users/register.html', {'form': form})
 
 
-def login(request):
-     form = LoginForm(request.POST or None)
-     return render(request, 'users/login.html', {'form': form})
+def login_page(request):
+    form = LoginForm(request.POST or None)
+    context = {
+        "form": form
+    }
+    if request.user.is_authenticated:
+
+        if form.is_valid():
+            print(form.cleaned_data)
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                context['form'] = LoginForm()
+                messages.success(request, f' {username} Successfully LOG IN !')
+                return redirect('blog-home')
+            else:
+                messages.error(request, " Invalid Password or UserName")
+                return redirect('/login_page')
+
+    return render(request, 'users/login.html', context)
