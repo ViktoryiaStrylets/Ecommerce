11bc3ea6d6a099ba262766957077c1e5595934b5
Max Wertheim <minecart123@aol.com>
2019-04-26 22:17:32 -0400

    Sign in button now redirects to login page. Also made a small grammar correction.

--- shop/urls.py
+++ shop/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.test, name='shopping test'),
+    path('', views.test, name='shopping-test'),
 ]
--- shop/views.py
+++ shop/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def test(request):
-    return render(request, 'blog/testview.html')
+    return render(request, 'shop/testview.html')
