d49fcdcc3263b6b24f30c64d1019119d1691dbfc
DESKTOP-859DUJQ\vikus <viktoryia.strylets@stonybrook.edu>
2019-05-02 14:41:09 -0400

    logo url

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -18,14 +18,13 @@ from django.conf.urls.static import static
 
 from django.contrib import admin
 from django.urls import path, include
-from users import views as user_views
-from shop import views as shop_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
     path('', include('blog.urls')),
     path('shop/', include('shop.urls')),
+    # path('shoppingcart/', include('shoppingcart.urls')),
 ]
 
 if settings.DEBUG:
