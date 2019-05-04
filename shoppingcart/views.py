8e0d5dacc52688a31a392b7909655f99dc2925ec
DESKTOP-859DUJQ\vikus <viktoryia.strylets@stonybrook.edu>
2019-05-03 23:37:20 -0400

    shopping cart app added

--- shoppingcart/views.py
+++ shoppingcart/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
-
-
-
+from.models import ShoppingCart
 
 def cart_home(request):
-
+    cart_id = request.session.get("cart_id", None)
+    cart_obj = ShoppingCart.objects.create()
+    print(cart_obj.id)
     return render(request, 'shoppingcart/cart.html', {})
 
